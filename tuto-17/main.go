c32208dcfb999207d83700f1859aa344f049b328
JArias <jarias@wawand.co>
2021-09-21 10:14:20 -0500

    User Input and Switch

---
+++ tuto-17/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
+
+	name, _ := getInput("Create a new bill name: ", reader)
+
+	b := newBill(name)
+	fmt.Println("Created the bill - ", b.name)
+
+	return b
+}
+
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+	opt, _ := getInput("Choose an option (a - add item, s - save a bill, t - add tip): ", reader)
+
+	switch opt {
+	case "a":
+		name, _ := getInput("Item name: ", reader)
+		price, _ := getInput("Item price: ", reader)
+		fmt.Println(name, price)
+
+	case "s":
+		fmt.Println("you chose s")
+
+	case "t":
+		tip, _ := getInput("Item name: ", reader)
+		fmt.Println(tip)
+
+	default:
+		fmt.Println("that was not a valid option...")
+		promptOptions(b)
+	}
+}
+
+func main() {
+	mybill := createBill()
+	promptOptions(mybill)
+
+	fmt.Println(mybill)
+}
